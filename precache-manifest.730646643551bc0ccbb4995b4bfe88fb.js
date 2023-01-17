95df52f7dda9cffbfca0c1006a67dd63ed5e5c42
Msshohruh <msshohruh@gmail.com>
2023-01-17 18:41:28 +0500

    Updates

---
+++ precache-manifest.730646643551bc0ccbb4995b4bfe88fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21488b265a611989f067420ae6b2c80",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a05386c60ed911a00521",
+    "url": "/test/static/css/main.0d1fa7e2.chunk.css"
+  },
+  {
+    "revision": "1b1f1f6d04f5ca36baa8",
+    "url": "/test/static/js/2.7785fa02.chunk.js"
+  },
+  {
+    "revision": "ab8f792973d32f9b93c607ee81282492",
+    "url": "/test/static/js/2.7785fa02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05386c60ed911a00521",
+    "url": "/test/static/js/main.8ab5592e.chunk.js"
+  },
+  {
+    "revision": "ac72963f3546a51212dc",
+    "url": "/test/static/js/runtime-main.cab68284.js"
+  }
+]);
